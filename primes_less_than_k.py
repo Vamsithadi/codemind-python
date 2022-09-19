9507d0540c478a7eba55c49c82bafac1e24f912b
Vamsithadi <102540592+Vamsithadi@users.noreply.github.com>
2022-09-19 15:35:17 +0530

    def pm(n):     c=0     for i in range(1,n//2+1):         if n%i==0:             c+=1     if c==1:         return True     else:         return False a=int(input()) l=list(map(int,input().split())) k=int(input()) d=0 for i in l:     if i<=k and pm(i)==True:         d+=1 print(d)

---
+++ primes_less_than_k.py
@@ -0,0 +1,17 @@
+def pm(n):
+    c=0
+    for i in range(1,n//2+1):
+        if n%i==0:
+            c+=1
+    if c==1:
+        return True
+    else:
+        return False
+a=int(input())        
+l=list(map(int,input().split()))
+k=int(input())
+d=0
+for i in l:
+    if i<=k and pm(i)==True:
+        d+=1
+print(d) 
